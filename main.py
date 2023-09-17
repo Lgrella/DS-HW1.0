f1780db6c686a20c3dbc4fc46f84fd7978055b6b
Lgrella <60951320+Lgrella@users.noreply.github.com>
2023-09-17 15:50:53 +0000

    main.py updates

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+'Pandas'
+import pandas as pd
+import matplotlib as plt
 
+df = pd.read_csv("/workspaces/DS-HW1.0/diabetes.csv")
+
+print(df.head())
+
+df2 = df[['Glucose', 'BloodPressure','SkinThickness','Insulin','BMI']]
+dff = df.copy()
+dff['MissingValues']=(df2 == 0).T.sum()
+
+print(dff.head())
+
+plt.hist(dff)
+plt.show()
