be274aa7a4b9068a879784d90af393137c53126d
Lgrella <60951320+Lgrella@users.noreply.github.com>
2023-09-19 15:10:01 +0000

    update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,2 +1,23 @@
+"""
+Created on Tue Sep 19 2023
+@author: lillygrella
+"""
+import unittest
+import pandas as pd
 
-assert True == True
+class TestMain(unittest.TestCase):
+    """
+    evoke unit testing to confirm readin and sumstats work correctly
+    """
+
+    def test_readin(self):
+        """
+        test that the readin function correctly populates df as a pd.dataframe datatype
+        """
+        diabetes = pd.read_csv("diabetes.csv")
+        self.assertIsNotNone(diabetes)
+        self.assertIsInstance(diabetes, pd.DataFrame)
+
+# Run the tests
+if __name__ == "__main__":
+    unittest.main()
